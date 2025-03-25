d5c5b7633ddb343ce8fb85df9f98c23ebde46dcf
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-03-25 15:58:11 +0300

    Add server.js and configure package.json for ES modules.

---
+++ hustleconnect-backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+//load enviroment variables
+dotenv.config();
+
+//Intialize express
+const app = express();
+
+//Middlewares
+app.use(express.json());// parse json data
+app.use(cors());//Enable cors for cross origin requests
+
+//Routes
+app.get('/', (req,res) => {
+    res.send("API is running...");
+});
+
+//Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
