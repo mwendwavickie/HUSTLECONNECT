a3e3e4619cd062a43da130f07b82760d3cb96e54
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-03-25 16:10:23 +0300

    Add MongoDB connection setup and connect with server.

--- hustleconnect-backend/server.js
+++ hustleconnect-backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './config/db.js';
 
 //load enviroment variables
 dotenv.config();
@@ -22,3 +23,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+// Connect to database
+connectDB();
