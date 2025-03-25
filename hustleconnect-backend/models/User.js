740f585c06c17109e84e4da39a18e72bcccf8237
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-03-25 16:17:44 +0300

    Add User model with schema  for authentication.

---
+++ hustleconnect-backend/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["customer", "vendor", "admin"], default: "customer" },
+        
+    },
+    { timestamps: true } // Automatically add createdAt and updatedAt fields
+);
+export default mongoose.model('user', UserSchema);
