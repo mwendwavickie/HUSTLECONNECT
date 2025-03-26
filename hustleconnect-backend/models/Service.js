349ea3fa14f204afaeed97f24212340c3b9aab6b
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-03-26 17:50:47 +0300

    Add Service model with schema for managing services

---
+++ hustleconnect-backend/models/Service.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import User from "./User";
+
+const serviceSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String, 
+            required: true
+        },
+        description: { 
+            type: String, required: true 
+        },
+        price: { 
+            type: Number, required: true 
+        },
+        category: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "Category", 
+            required: true 
+        },
+        User: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "User", 
+            required: true 
+        },
+        location: { 
+            type: String, 
+            required: true 
+        },
+        contact: { 
+            type: String, 
+            required: true 
+        },
+        available: { 
+            type: Boolean, 
+            default: true 
+        },
+    },
+    { timestamps: true }
+);
+export default mongoose.model("Service", serviceSchema);
