34a22bbb09a1c19f401b823f9404aa7d270328e9
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-05-06 13:53:53 +0300

    Add routes for retrieving and updating bookings with role-based access control

--- hustleconnect-backend/routes/bookingRoute.js
+++ hustleconnect-backend/routes/bookingRoute.js
@@ -31,5 +31,58 @@ router.post('/book', authMiddleware, async (req, res) => {
     }
 });
 
+// @route   GET /api/bookings
+// @desc    Get all bookings (Customer sees their bookings, Vendor sees their received bookings)
+// @access  Private (Customer/Vendor)
+
+router.get('/bookings', authmiddleware, async (req, res) => {
+    try {
+        let bookings;
+        if (req.user.role === 'customer'){
+            bookings = await Booking.find({ customer: req.user.id}) .populate('service', "title price").populate('vendor', 'name email');
+        } else if (req.user.role === "vendor"){
+            bookings =  await Booking.find({ vendor: req.user.id}).populate('service', "title price").populate('customer', 'name email');  
+        } else {
+            return res.status(403).json({ message: " Unauthorized access"});
+        }
+        res.status(200).json(bookings);
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Internal server error"});
+        }
+    }
+);
+
+// @route   PUT /api/bookings/:id
+// @desc    Update booking status (Vendor only)
+// @access  Private (Vendor)
+router.put('/bookings/:id', authMiddleware, async (req, res) => {
+    if (!req.user.role === "vendor"){
+        return res.status(403).json({ message: "Only vendors can update booking status"});
+    }
+    try{
+        const { status } = req.body;
+        const booking =  await Booking.findById(req.params.id);
+
+        if (!booking){
+            return res.status(404).json({ message: 'Booking not found'});
+        }
+        if (booking.vendor.toString() !== req.user.id){
+            return res.status(403).json({ message: "Unauthorized "});
+        }
+        if (!["approved", "completed", "cancelled"].includes(status)){
+            return res.status(400).json({ message: "Invalid status update."});
+        }
+
+        booking.status = status;
+        await booking.save();
+        res.json(booking);
+        } catch (error) {
+            res.status(500).json({ message: "Internal server errror"});
+        console.error(error);
+        }
+    });
+
 
 export default router;
