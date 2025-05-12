79e2a8952d2b9501fa00a8c242852f8c62fec7de
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-05-12 15:15:25 +0300

    Add navigation structure to include BottomTab navigator.

--- hustleconnect-frontend/App.js
+++ hustleconnect-frontend/App.js
@@ -3,14 +3,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTab from './navigation/BottomTab';
 import HomeScreen from './screens/HomeScreen';
+import ServiceScreen from './screens/ServiceScreen';
 
 const Stack = createNativeStackNavigator(); 
 
 export default function App() {
   return (
       <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={HomeScreen} /> 
+      <Stack.Navigator initialRouteName='Main' screenOptions={{headerShown: false}}>
         <Stack.Screen name="Main" component={BottomTab}  />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ hustleconnect-frontend/navigation/BottomTab.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import ServiceScreen from "../screens/ServiceScreen";
+//import BookingScreen from "../screens/BookingScreen";
+//import ProfileScreen from "../screens/ProfileScreen";
+import { Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTab = () => {
+    return(
+        <Tab.Navigator
+        screenOptions={({route}) => ({
+            tabBarIcon: ({ color, size }) => {
+                let iconName;
+                switch (route.name) {
+                    case 'Home': iconName = 'home'; break;
+                    case 'Services': iconName = 'briefcase'; break;
+                    //case 'Booking': iconName = 'calendar'; break;
+                    //case 'Profile': iconName = 'person'; break;
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+        })}
+        
+        >
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Services" component={ServiceScreen} />
+            
+        </Tab.Navigator>
+    );
+}
+export default BottomTab;
