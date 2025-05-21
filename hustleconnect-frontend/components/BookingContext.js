23fc848fac1187a8cb6decd568c1a5fc89b90c8c
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-05-21 22:09:51 +0300

    Add BookingContext for managing booking state and functionality

---
+++ hustleconnect-frontend/components/BookingContext.js
@@ -0,0 +1,19 @@
+import react, {useState, createContext} from 'react';
+
+export const BookingContext = createContext();
+
+export const BookingProvider = ({ children }) => {
+    const [bookings, setBookings] = useState([]);
+
+    const addBooking = (booking) => {
+        setBookings((prevBookings) => [...prevBookings, booking]);
+    };
+    const removeBooking = (bookingId) => {
+        setBookings((prevBookings) => prevBookings.filter(booking => booking.id !== bookingId));
+    };
+    return(
+        <BookingContext.Provider value={{bookings, addBooking, removeBooking}}>
+            {children}
+        </BookingContext.Provider>
+    );
+};
