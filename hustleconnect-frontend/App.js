960588a93db47a5fad6f85c62f51ca8ecdb241b6
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-05-27 15:51:40 +0300

    Wrap application in AuthProvider and BookingProvider; add navigation for ServiceDetail, Signup, and Login screens

--- hustleconnect-frontend/App.js
+++ hustleconnect-frontend/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTab from './navigation/BottomTab';
-import HomeScreen from './screens/HomeScreen';
-import ServiceScreen from './screens/ServiceScreen';
+import ServiceDetailScreen from './screens/ServiceDetailScreen';
+import SignupScreen from './screens/SignupScreen';
+import LoginScreen from './screens/LoginScreen';
+import { BookingProvider } from './context/BookingContext';
+import { AuthProvider } from './context/AuthContext';
+
 
 const Stack = createNativeStackNavigator(); 
 
 export default function App() {
   return (
-      <NavigationContainer>
-      <Stack.Navigator initialRouteName='Main' screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Main" component={BottomTab}  />
-      </Stack.Navigator>
-    </NavigationContainer>
+    
+      <AuthProvider>
+       <BookingProvider>
+        <NavigationContainer>
+         <Stack.Navigator initialRouteName='Main' screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Main" component={BottomTab}  />
+          <Stack.Screen name="ServiceDetail" component={ServiceDetailScreen} options={{headerShown: true, title:'Service Detail'}} />
+          <Stack.Screen name="Signup" component={SignupScreen} options={{headerShown: true, title:'Sign Up'}} />
+          <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: true, title:'Login'}} />
+         </Stack.Navigator>
+        </NavigationContainer> 
+      </BookingProvider> 
+      </AuthProvider>
     
     
   );
