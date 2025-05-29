f8a9d4801ae45c30006dc10c9ba6be228651a6a9
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-05-29 12:55:53 +0300

    Enhance AuthContext to persist user data with AsyncStorage and manage authentication state

--- hustleconnect-frontend/context/AuthContext.js
+++ hustleconnect-frontend/context/AuthContext.js
@@ -1,12 +1,38 @@
-import React,{useState, createContext} from "react";
+import React,{useState, createContext, useEffect} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
+    const [isAutheticated, setIsAuthenticated] = useState(true);
 
-    const login = (userData) => setUser(userData);
-    const logout = () => setUser(null);
+    useEffect(() => {
+        const loadUserData = async () => {
+            try {
+                const userData = await AsyncStorage.getItem('user');
+                if (userData) {
+                    setUser(JSON.parse(userData));
+                }
+            } catch (error) {
+                console.error("Failed to load user data", error);
+            }finally {
+                setIsAuthenticated(false);
+            }
+        };
+        loadUserData();
+    }, []);
+    
+
+    const login = async (userData) => {
+        setUser(userData);
+        await AsyncStorage.setItem('user', JSON.stringify(userData));
+    };
+
+    const logout = async () => {
+        setUser(null);
+        await AsyncStorage.removeItem('user');
+    };
 
     return (
         <AuthContext.Provider value={{user, login, logout, isAutheticated: !!user}}>
