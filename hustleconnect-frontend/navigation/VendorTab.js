91ffcd06fbbef93d8a2658c56d59bbef1501f840
VICKIE MWENDWA <mwendwavickie@gmail.com>
2025-06-03 22:54:11 +0300

    Refactor VendorTab to implement a stack navigator and enhance tab navigation with dynamic icons

--- hustleconnect-frontend/navigation/VendorTab.js
+++ hustleconnect-frontend/navigation/VendorTab.js
@@ -1,32 +1,53 @@
-import react from "react";
+import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AddServiceScreen from "../screens/AddServiceScreen";
 import VendorBookingScreen from "../screens/VendorBookingScreen";
+import HomeScreen from "../screens/HomeScreen";
+import ServiceDetailScreen from "../screens/ServiceDetailScreen";
+
 import { Ionicons } from "@expo/vector-icons";
 
+
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const VendorStack = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="ServiceDetail" component={ServiceDetailScreen} />
+            <Stack.Screen name="VendorTab" component={VendorTab} />
+        </Stack.Navigator>
+    );
+}
 
 const VendorTab = () => {
     return (
-        <Tab.Navigator screenOptions={{ headerShown: false}}>
-            <Tab.Screen 
-                name="AddService"
-                component={AddServiceScreen}
-                options={{
-                    tabBarLabel: "Add Service",
-                    tabBarIcon: ({ color, size }) => <Ionicons name="add-circle-outline" size={size} color={color} />,
-                }}
-                />
-                <Tab.Screen 
-                name="MyBookings"
-                component={VendorBookingScreen}
-                options={{
-                    tabBarLabel: "My Bookings",
-                    tabBarIcon: ({ color, size }) => <Ionicons name="bookmarks-outline" size={size} color={color} />,
-                }}
-                />
-                
+        <Tab.Navigator 
+        screenOptions={({route})=> ({
+            tabBarIcon: ({color, size}) => {
+                let iconName;
 
+                if (route.name === "Dashboard") {
+                    iconName = "home-outline";
+                } else if (route.name === "AddService") {
+                    iconName = "add-circle-outline";
+                } else if (route.name === "MyBookings") {
+                    iconName = "bookmarks-outline";
+                }
+
+                return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            headerShown: false,
+            tabBarActiveTintColor: "#ff9900",
+            tabBarInactiveTintColor: "gray",
+        })}
+        >
+            <Tab.Screen name="Dashboard" component={VendorStack} />
+            <Tab.Screen name="AddService" component={AddServiceScreen} />
+            <Tab.Screen name="MyBookings" component={VendorBookingScreen} />
+            
         </Tab.Navigator>
     );
 };
